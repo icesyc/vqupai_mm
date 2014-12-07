9987bd85e2f03ae8501cf068e15e553bb116d44a
icesyc <icesyc@gmail.com>
2014-12-07 16:01:20 +0800

    调整发布脚本

--- pub.sh
+++ pub.sh
@@ -10,12 +10,20 @@ else
 fi
 echo "publish to $HOST:"
 
-scp -r protected/controllers root@115.28.134.105:/alidata/www/vqupai/$HOST/protected
-scp -r protected/models root@115.28.134.105:/alidata/www/vqupai/$HOST/protected
-scp -r protected/views root@115.28.134.105:/alidata/www/vqupai/$HOST/protected
-scp -r protected/components root@115.28.134.105:/alidata/www/vqupai/$HOST/protected
-scp -r $INDEX_FILE root@115.28.134.105:/alidata/www/vqupai/$HOST/index.php
-scp -r protected/config/$CONFIG_FILE root@115.28.134.105:/alidata/www/vqupai/$HOST/protected/config/main.php
-#scp -r protected/components root@115.28.134.105:/alidata/www/vqupai/$HOST/protected
-#scp -r protected/extensions root@115.28.134.105:/alidata/www/vqupai/$HOST/protected
+if [[ "$2" == "full" ]];then
+	mv protected/config/main.php protected/config/main.php.bak
+	cp protected/config/production.php protected/config/main.php
+	scp -r . root@115.28.134.105:/alidata/www/vqupai/$HOST/
+	scp -r $INDEX_FILE root@115.28.134.105:/alidata/www/vqupai/$HOST/index.php
+	mv protected/config/main.php.bak protected/config/main.php
+else
+	scp -r protected/controllers root@115.28.134.105:/alidata/www/vqupai/$HOST/protected/
+	scp -r protected/models root@115.28.134.105:/alidata/www/vqupai/$HOST/protected/
+	scp -r protected/views root@115.28.134.105:/alidata/www/vqupai/$HOST/protected/
+	scp -r protected/components root@115.28.134.105:/alidata/www/vqupai/$HOST/protected/
+	scp -r $INDEX_FILE root@115.28.134.105:/alidata/www/vqupai/$HOST/index.php
+	scp -r protected/config/$CONFIG_FILE root@115.28.134.105:/alidata/www/vqupai/$HOST/protected/config/main.php
+	#scp -r protected/components root@115.28.134.105:/alidata/www/vqupai/$HOST/protected
+	#scp -r protected/extensions root@115.28.134.105:/alidata/www/vqupai/$HOST/protected
+fi
 

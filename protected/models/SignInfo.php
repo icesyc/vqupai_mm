758ad8ca5f39a827afa493b41dd6f217b89079bf
textblank <4296851@qq.com>
2014-12-14 21:34:49 +0800

    用户签到

--- protected/models/SignInfo.php
+++ protected/models/SignInfo.php
@@ -1,26 +1,25 @@
 <?php
 
 /**
- * This is the model class for table "coupon".
+ * This is the model class for table "sign_info".
  *
- * The followings are the available columns in table 'coupon':
- * @property string $id
- * @property string $name
- * @property string $pic_url
- * @property string $description
- * @property string $value
- * @property integer $is_bind
- * @property integer $disabled
- * @property string $expire_time
+ * The followings are the available columns in table 'sign_info':
+ * @property integer $id
+ * @property string $month
+ * @property integer $day
+ * @property integer $exp
+ * @property integer $score
+ * @property integer $coupon
+ * @property integer $prop
  */
-class Coupon extends CActiveRecord
+class SignInfo extends CActiveRecord
 {
 	/**
 	 * @return string the associated database table name
 	 */
 	public function tableName()
 	{
-		return 'coupon';
+		return 'sign_info';
 	}
 
 	/**
@@ -31,15 +30,11 @@ class Coupon extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('name, pic_url, description', 'required'),
-			array('is_bind, disabled', 'numerical', 'integerOnly'=>true),
-			array('name', 'length', 'max'=>32),
-			array('pic_url', 'length', 'max'=>128),
-			array('description', 'length', 'max'=>256),
-			array('value, expire_time', 'length', 'max'=>10),
+			array('day, exp, score, coupon, prop', 'numerical', 'integerOnly'=>true),
+			array('month', 'length', 'max'=>6),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('id, name, pic_url, description, value, is_bind, disabled, expire_time', 'safe', 'on'=>'search'),
+			array('id, month, day, exp, score, coupon, prop', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -60,14 +55,13 @@ class Coupon extends CActiveRecord
 	public function attributeLabels()
 	{
 		return array(
-			'id' => '自增id',
-			'name' => '拍券名称',
-			'pic_url' => '拍券的图片url',
-			'description' => '拍券的描述',
-			'value' => '拍券的金额',
-			'is_bind' => '是否绑定',
-			'disabled' => '是否被禁用 0 未禁用 1 已禁用',
-			'expire_time' => '过期时间',
+			'id' => 'id',
+			'month' => '月份',
+			'day' => '日',
+			'exp' => '经验值',
+			'score' => '积分',
+			'coupon' => '拍券',
+			'prop' => '道具',
 		);
 	}
 
@@ -89,40 +83,44 @@ class Coupon extends CActiveRecord
 
 		$criteria=new CDbCriteria;
 
-		$criteria->compare('id',$this->id,true);
-		$criteria->compare('name',$this->name,true);
-		$criteria->compare('pic_url',$this->pic_url,true);
-		$criteria->compare('description',$this->description,true);
-		$criteria->compare('value',$this->value,true);
-		$criteria->compare('is_bind',$this->is_bind);
-		$criteria->compare('disabled',$this->disabled);
-		$criteria->compare('expire_time',$this->expire_time,true);
+		$criteria->compare('id',$this->id);
+		$criteria->compare('month',$this->month,true);
+		$criteria->compare('day',$this->day);
+		$criteria->compare('exp',$this->exp);
+		$criteria->compare('score',$this->score);
+		$criteria->compare('coupon',$this->coupon);
+		$criteria->compare('prop',$this->prop);
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
 		));
 	}
-	
-	public function getExpireTime($from=null){
-		!$from && $from = time();
-		return $this->expire_time > 0 ? $from + $this->expire_time * 86400 : 0;
-	}
 
-	public function getAllText()
+	public function getSignInfoMonth($month)
 	{
-		$rows = self::model()->findAll();
-		$ret = array();
-		foreach($rows as $row){
-			$ret[$row->id] = $row->name;
+		$signs = self::model()->findAllByAttributes(array('month' => $month));
+		if($signs){
+			$info = array();
+			foreach($signs as $r){
+				$info[] = array(
+					'exp'=>$r->exp,
+					'score'=>$r->score,
+					'coupon'=>$r->coupon,
+					'prop'=>$r->prop,
+					);
+			}
+			return $info;
 		}
-		return $ret;
+		else{
+			return false;
+		}
 	}
 
 	/**
 	 * Returns the static model of the specified AR class.
 	 * Please note that you should have this exact method in all your CActiveRecord descendants!
 	 * @param string $className active record class name.
-	 * @return Coupon the static model class
+	 * @return SignInfo the static model class
 	 */
 	public static function model($className=__CLASS__)
 	{
